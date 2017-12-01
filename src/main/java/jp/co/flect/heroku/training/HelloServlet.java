1cb0ea184ad2a04cb6f51ae11578ff7eb6f73d2d
eyoshinaga <eyoshinaga@value-p.com>
2017-12-01 13:24:18 +0900

    first commit

---
+++ src/main/java/jp/co/flect/heroku/training/HelloServlet.java
@@ -0,0 +1,22 @@
+package jp.co.flect.heroku.training;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name="HelloServlet", urlPatterns={"/"})
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("TEST_VARIABLE   : " + System.getenv("TEST_VARIABLE"));
+		System.out.println("Heroku App PORT   : " + System.getenv("PORT"));
+		res.getWriter().println("TEST_VARIABLE   : " + System.getenv("TEST_VARIABLE"));
+		res.getWriter().println("Heroku App PORT   : " + System.getenv("PORT"));
+		res.getWriter().println("Hello World! develop1 update-5");
+	}
+}
