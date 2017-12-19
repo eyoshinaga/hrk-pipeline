cb5b228a05203b59a5c719b97721dad304037299
yoshinaga <yoshinaga>
2017-12-19 19:08:16 +0900

    chang to yellow 18

--- src/main/java/jp/co/flect/heroku/training/HelloServlet.java
+++ src/main/java/jp/co/flect/heroku/training/HelloServlet.java
@@ -17,6 +17,6 @@ public class HelloServlet extends HttpServlet {
 		System.out.println("Heroku App PORT   : " + System.getenv("PORT"));
 		res.getWriter().println("<h1 >TEST_VARIABLE   : <font color=\"yellow\">" + System.getenv("TEST_VARIABLE")+ "</font></h1>");
 		res.getWriter().println("Heroku App PORT   : " + System.getenv("PORT"));
-		res.getWriter().println("Hello World! develop3 update-17");
+		res.getWriter().println("Hello World! develop3 update-18");
 	}
 }
